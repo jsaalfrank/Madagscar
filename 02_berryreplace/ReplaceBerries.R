bd664e1f4b0ef51e68d7950a4b5e3883c6c48fc5
JohannaSaal <154228320+JohannaSaali@users.noreply.github.com>
2024-04-17 11:03:33 +0200

    Delete 02_berryreplace directory

--- 02_berryreplace/ReplaceBerries.R
+++
@@ -1,4 +0,0 @@
-library("data.table")
-berrytable<-fread("../00_RawData.ln/berries.txt")
-berrytable[,berry:=gsub("berry","",berry)]
-fwrite(berrytable,file="../localresults.ln/replaced_berries_result.txt")
